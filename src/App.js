9eb17100e53a7a65e9567aa69bcb0067ab5b3a61
Abdul Hanan Sharif <146536242+hanansharif@users.noreply.github.com>
2023-12-02 07:46:48 +0500

    context API

--- src/App.js
+++ src/App.js
@@ -7,17 +7,22 @@ import {
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
+import NoteState from './context/notes/NotesState';
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
     </>
   );
 }
