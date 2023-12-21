16dfcfa1610a00d15fe5b80a264a703965873bb7
Abdul Hanan Sharif <146536242+hanansharif@users.noreply.github.com>
2023-12-21 23:35:37 +0500

    icons

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/NotesState';
+import Alert from './components/Alert';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <NoteState>
         <Router>
           <Navbar />
+          <Alert message="this is an alert" />
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home />} />
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,12 +4,12 @@ import Noteitem from './Noteitem';
 
 const Notes = () => {
     const context = useContext(noteContext)
-    const { notes, setNotes } = context;
+    const { notes } = context;
     return (
         <div className="row my-3">
             <h2>Your Notes</h2>
             {notes.map((note) => {
-                return <Noteitem note={note} />
+                return <Noteitem key={note._id} note={note} />
             })}
         </div>
     )
