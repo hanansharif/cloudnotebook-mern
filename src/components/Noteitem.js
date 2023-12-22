21ef808c284cbc652d894186df079e0f49ed2639
Abdul Hanan Sharif <146536242+hanansharif@users.noreply.github.com>
2023-12-22 12:31:34 +0500

    fetch Notes

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import noteContext from '../context/notes/noteContext'
 
 const Noteitem = (props) => {
+    const context = useContext(noteContext)
+    const { deleteNote } = context;
     const { note } = props;
     return (
         <div className='col-md-3'>
@@ -9,7 +12,7 @@ const Noteitem = (props) => {
                     {/* <div className="d-flex align-items-center"> */}
                     <div className="d-flex">
                         <h5 className="col-9 card-title">{note.title}</h5>
-                        <i className="far fa-trash-alt mx-2"></i>
+                        <i className="far fa-trash-alt mx-2" onClick={() => { deleteNote(note._id) }}></i>
                         <i className="fas fa-edit mx-2"></i>
                     </div>
                     <p className="card-text">{note.description}</p>
